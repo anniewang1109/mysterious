a6986f91a375073a49b92d22269fdca89d684aa1
themost1 <benjaminmost@gmail.com>
2018-10-12 15:59:47 -0400

    Added tile graphic and movement/grid code

--- main.lua
+++ main.lua
@@ -22,15 +22,21 @@ Use the keyword "local" when you don't want a global variable (i.e., most of the
 
 --function that is called automatically on program load
 function love.load()
-	num = 1
+	--love.window.setMode(100, 100, {})
+
+	player = {
+		xCoord = 0,
+		yCoord = 0,
+		inventory = {}
+	}
+
 end
 
 
 --update function called every frame
 function love.update(dt)
-	num = num+1
 	--print with print function (like System.out.print, print in Python, etc.)
-	print(num)
+	--print(num)
 
 	--[[
 
@@ -45,17 +51,55 @@ function love.update(dt)
 
 
 	]]
+
+	local velX = 55
+	local velY = 55
+
+	if love.keyboard.isDown("up") then
+		player.yCoord = player.yCoord - (velY * dt)
+	end
+	if love.keyboard.isDown("down") then
+		player.yCoord = player.yCoord + (velY * dt)
+	end
+	if love.keyboard.isDown("left") then
+		player.xCoord = player.xCoord - (velX * dt)
+	end
+	if love.keyboard.isDown("right") then
+		player.xCoord = player.xCoord + (velX * dt)
+	end
 end
 
 
 --draw function called every frame
 function love.draw()
 	--set background color
-	love.graphics.setBackgroundColor(240, 0, 0)
+	
+	local tileWidth = 50
+	local tileHeight = 50
+	local roomWidth = 10
+	local roomHeight = 10
 
-	local character = util.getImage("graphics/herman.png")
+	local tile = util.getImage("graphics/tile.png")
 	--draw takes parameters: image, x, y, rotation, scaleX, scaleY
 	--can take two more at end, but these are pretty irrelevant
-	love.graphics.draw(character, 100, 100, 0, 1, 1)
 
+	--[[ eventual room matrix:
+	room = {
+		{tile1, tile2, tile3},
+		{tile4, tile5, tile6}
+	}
+	]]
+
+	-- equivalent of (for int row = 0; row < roomHeight; row++)
+	for row = 0, roomHeight-1 do
+		for col = 0, roomWidth-1 do
+			love.graphics.draw(tile, row*tileWidth, col*tileHeight, 0,
+				tileWidth/tile:getWidth(), tileHeight/tile:getHeight())
+		end
+	end
+
+	--draw player
+	local playerSprite = util.getImage("graphics/herman.png")
+	love.graphics.draw(playerSprite, player.xCoord, player.yCoord, 0,
+		tileWidth/playerSprite:getWidth(), tileHeight/playerSprite:getHeight())
 end
